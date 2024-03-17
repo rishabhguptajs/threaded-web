670cd686b3f080ea2e89cf84f54e392f4d7d1a87
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-03-17 14:08:49 +0530

    worked on the homepage

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Features = ({ name, description, logoURL }) => {
   return (
-    <div className="p-4 w-[25%] transition-all cursor-pointer hover:bg-[#8793ff] rounded-lg text-white border-2 border-[#8793ff]">
+    <div className="p-4 w-[25%] transition-all cursor-pointer hover:bg-[#8793ff] hover:text-[#071137] rounded-lg text-white border-2 border-[#8793ff]">
       <div className="flex flex-row items-center text-center justify-left">
         <span className="p-2 m-2 bg-[#8793ff] text-white rounded-full align-middle justify-center items-center">
           {logoURL}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,31 +16,25 @@ const HomePage = () => {
         <Header />
         <AnimatedCircle />
       </span>
-      <main className="w-[100vw] h-[100vh] flex flex-col align-middle text-center justify-center items-center mb-20">
-        <div>
-          <video
-            width="70%"
-            autoPlay
-            muted
-            loop
-            className="relative top-[-3em] left-[12em] rounded-[30px] m-0 p-0"
-          >
-            <source src={video} type="video/mp4" />
-            your browser doesn't support
-          </video>
-          <div className="text-[30px] text-white font-bold font-sans">
-            Low-end, with the power of CLI <br />{" "}
-            <span className="text-[14px] font-medium flex flex-col">
-              Specially helpful for low-end PCs and{" "}
-              <span>fo the users who're into linux.</span>
-            </span>
-          </div>
-          <button className=" m-8 p-4 bg-white font-semibold text-black transition-all rounded-xl hover:-translate-y-1 hover:scale-105">
-            Get Started.
-          </button>
+      <div className="relative top-[-8em] text-center w-fit justify-center align-middle">
+        <video width="70%" autoPlay muted loop className="rounded-[30px]">
+          <source src={video} type="video/mp4" />
+          your browser doesn't support
+        </video>
+        <div className="text-[30px] text-white font-bold font-sans">
+          Low-end, with the power of CLI <br />{" "}
+          <span className="text-[14px] font-medium flex flex-col">
+            Specially helpful for low-end PCs and{" "}
+            <span>fo the users who're into linux.</span>
+          </span>
         </div>
+        <button className=" m-8 p-4 bg-white font-semibold text-black transition-all rounded-xl hover:-translate-y-1 hover:scale-105">
+          Get Started.
+        </button>
+      </div>
+      <main className="w-[100vw] flex flex-col align-middle text-center justify-center items-center mb-20">
         <div className="features-section justify-evenly flex flex-col gap-4 m-8 p-4 bg-[#07113767]">
-          <span className="text-white z-50 transition-all text-4xl m-4 hover:text-[#8793ff] cursor-pointer">
+          <span className="text-white transition-all text-4xl m-4 hover:text-[#8793ff] cursor-pointer">
             Features
           </span>
           <div className="flex flex-row gap-4">
@@ -74,11 +68,43 @@ const HomePage = () => {
             />
           </div>
         </div>
-        <div className="">
-          <span className="text-white">How It Works?</span>
+        <div>
+          <div className="text-white text-4xl font-semibold font-sans">
+            How It Works?
+          </div>
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col w-[30%] m-4 p-4 bg-[#08144592] rounded-xl">
+              <h1 className="text-white text-2xl font-semibold">Step 1</h1>
+              <span className="text-white text-lg">
+                Install the CLI tool using pip from{" "}
+                <a
+                  href="https://pypi.org/project/threaded-cli/"
+                  target="_blank"
+                  className="text-blue-400"
+                >
+                  pypi
+                </a>
+                .
+              </span>
+            </div>
+            <div className="flex flex-col w-[30%] m-4 p-4 bg-[#08144592] rounded-xl">
+              <h1 className="text-white text-2xl font-semibold">Step 2</h1>
+              <span className="text-white text-lg">
+                Authenticate your Twitter account using the CLI tool.
+              </span>
+            </div>
+            <div className="flex flex-col w-[30%] m-4 p-4 bg-[#08144592] rounded-xl">
+              <h1 className="text-white text-2xl font-semibold">Step 3</h1>
+              <span className="text-white text-lg">
+                Start creating threads and tweeting from your terminal.
+              </span>
+            </div>
+          </div>
         </div>
       </main>
-      <Footer />
+      <div className="relative bottom-0 mt-40">
+        <Footer />
+      </div>
     </div>
   )
 }
